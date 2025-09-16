d4def49e28fc157fa95643cee19a891628ebc1f6
Le Gia Uy <leuy091104@gmail.com>
2025-09-16 23:03:02 +0700

    gix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import Footer from "./features/layout/Footer/Footer.jsx";
-import FlashsaleSection from "./features/layout/Flashsale/FlashsaleSection.jsx";
+import Footer from "./features/layout/Footer/Footer";
+
+import { AppRouter } from "./features/routes/AppRouter";
+
+import { Toaster } from "sonner";
 
 function App() {
   return (
     <>
-      <FlashsaleSection />
+      <AppRouter />
       <Footer />
+      <Toaster richColors position="top-right" />
     </>
   );
 }
