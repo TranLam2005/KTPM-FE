a598247531c180ca13fa5c82d80ca6d43007e169
Le Gia Uy <leuy091104@gmail.com>
2025-09-16 21:37:02 +0700

    Add Flashsale Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import Footer from "./components/Footer";
+import Footer from "./layout/components/Footer/Footer.jsx";
+import FlashsaleSection from "./layout/components/Flashsale/FlashsaleSection.jsx";
 
 function App() {
   return (
     <>
+      <FlashsaleSection />
       <Footer />
     </>
   );
